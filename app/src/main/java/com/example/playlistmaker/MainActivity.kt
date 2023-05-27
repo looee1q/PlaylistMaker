6396962065e1d1619c7f8855853219b117dfcd94
looee1q <valera.sevostyanov@yandex.ru>
2023-05-27 13:59:26 +0300

    Sprint 9. Homework. FirstPR

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -12,24 +12,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val searchButton = findViewById<Button>(R.id.search_button)
         val musicLibraryButton = findViewById<Button>(R.id.music_library_button)
         val settingsButton = findViewById<Button>(R.id.settings_button)
-        val searchButtonClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                //Toast.makeText(this@MainActivity, "Вы нажали на кнопку \"Поиск\"", Toast.LENGTH_SHORT).show()
-                val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
-                startActivity(displayIntent)
-            }
+
+        searchButton.setOnClickListener {
+            val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(displayIntent)
         }
-        searchButton.setOnClickListener(searchButtonClickListener)
+
         musicLibraryButton.setOnClickListener {
-            //Toast.makeText(this, "Вы нажали на кнопку \"Медиатека\"", Toast.LENGTH_SHORT).show()
             val displayIntent = Intent(this@MainActivity, MusicLibraryActivity::class.java)
             startActivity(displayIntent)
         }
+
         settingsButton.setOnClickListener {
-            //Toast.makeText(this, "Вы нажали на кнопку \"Настройки\"", Toast.LENGTH_SHORT).show()
             val displayIntent = Intent(this@MainActivity, SettingsActivity::class.java)
             startActivity(displayIntent)
         }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,19 +1,64 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         val backToMainScreen = findViewById<ImageView>(R.id.to_main_screen_from_settings)
+        val switch = findViewById<SwitchCompat>(R.id.night_mode_switch)
+        val shareAppIcon = findViewById<ImageView>(R.id.share_app_icon)
+        val writeToTheSupportIcon = findViewById<ImageView>(R.id.write_to_the_support_icon)
+        val userAgreementIcon = findViewById<ImageView>(R.id.user_agreement_icon)
+
         backToMainScreen.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             finish()
             startActivity(displayIntent)
         }
+
+        switch.setOnCheckedChangeListener { button, isChecked ->
+            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        shareAppIcon.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.link_to_user_agreement))
+            }
+            val chooser: Intent = Intent.createChooser(shareIntent, "")
+            startActivity(chooser)
+        }
+
+        writeToTheSupportIcon.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_default_recipient)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_default_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_default_text))
+            }
+            startActivity(sendIntent)
+        }
+
+        userAgreementIcon.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(getString(R.string.link_to_offer))
+                addCategory(Intent.CATEGORY_BROWSABLE)
+            }
+            startActivity(sendIntent)
+        }
+
     }
 }
