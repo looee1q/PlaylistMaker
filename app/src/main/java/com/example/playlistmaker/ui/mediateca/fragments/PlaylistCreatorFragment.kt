d10a2e5ffba1956294d1e715953f9949b6a8244c
looee <transistor42b@yandex.ru>
2023-12-27 18:08:20 +0300

    Sprint 22. Добавил появление DialogAlert для подтверждения выхода из PlaylistCreatorFragment. Добавил появление toast-сообщения после создания плейлиста. Добавил возможность обратной навигаци с PlaylistCreatorFragment по кнопке интерфейса

--- app/src/main/java/com/example/playlistmaker/ui/mediateca/fragments/PlaylistCreatorFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediateca/fragments/PlaylistCreatorFragment.kt
@@ -1,22 +1,30 @@
 package com.example.playlistmaker.ui.mediateca.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentCreateNewPlaylistBinding
 import com.example.playlistmaker.roundedCorners
-
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 class PlaylistCreatorFragment : Fragment() {
 
     private var _binding: FragmentCreateNewPlaylistBinding? = null
     private val binding get() = _binding!!
+
+    private val dialog by lazy { createConfirmPlaylistCreationDialog() }
 
     private val photoLauncher = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()) { photoUri ->
@@ -28,6 +36,11 @@ class PlaylistCreatorFragment : Fragment() {
         } else {
             Log.d("PhotoLauncher", "No cover selected")
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("PlaylistCreatorFragment","onCreate")
     }
 
     override fun onCreateView(
@@ -46,18 +59,56 @@ class PlaylistCreatorFragment : Fragment() {
             photoLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
-        binding.createNewPlaylistButton.setOnClickListener {
-
+        binding.playlistNameTextInputEditText.doOnTextChanged { text, start, before, count ->
+            binding.createNewPlaylistButton.isEnabled = text?.isNotBlank() == true
         }
+
+        binding.createNewPlaylistButton.setOnClickListener {
+            val playlistName = binding.playlistNameTextInputEditText.text.toString()
+            Toast.makeText(requireContext(), "Плейлист $playlistName создан", Toast.LENGTH_SHORT).show()
+            findNavController().navigateUp()
+        }
+
+        binding.backArrowButton.setOnClickListener {
+            dialog.show()
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback( object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                dialog.show()
+            }
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-    companion object {
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("PlaylistCreatorFragment","onDestroy")
+    }
+
+    private fun createConfirmPlaylistCreationDialog(): MaterialAlertDialogBuilder {
+        val dialogListener = DialogInterface.OnClickListener { _, which ->
+            when (which) {
+                DialogInterface.BUTTON_POSITIVE -> {
+                    findNavController().navigateUp()
+                }
+                DialogInterface.BUTTON_NEUTRAL -> null
+            }
+        }
+        return MaterialAlertDialogBuilder(requireContext())
+            .setTitle(resources.getString(R.string.finish_playlist_creation))
+            .setMessage(resources.getString(R.string.all_unsaved_data_will_be_lost))
+            .setPositiveButton(resources.getString(R.string.finish), dialogListener)
+            .setNeutralButton(resources.getString(R.string.cancel), dialogListener)
+    }
+
+    companion object {
         private const val RADIUS_OF_PLAYLIST_COVER_CORNERS: Float = 8f
     }
 
 }
+
