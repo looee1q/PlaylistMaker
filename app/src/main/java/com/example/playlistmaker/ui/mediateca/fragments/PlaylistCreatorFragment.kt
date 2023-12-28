31cafdb864b0bf449bd444f5e891089e209b0af2
looee <transistor42b@yandex.ru>
2023-12-28 12:56:27 +0300

    Sprint 22. Добавил опцию сохранения картинки обложки плейлиста во внешнее хранилище при создании плейлиста

--- app/src/main/java/com/example/playlistmaker/ui/mediateca/fragments/PlaylistCreatorFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediateca/fragments/PlaylistCreatorFragment.kt
@@ -1,7 +1,11 @@
 package com.example.playlistmaker.ui.mediateca.fragments
 
 import android.content.DialogInterface
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +14,7 @@ import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -19,12 +24,17 @@ import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentCreateNewPlaylistBinding
 import com.example.playlistmaker.roundedCorners
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.io.File
+import java.io.FileOutputStream
+
 class PlaylistCreatorFragment : Fragment() {
 
     private var _binding: FragmentCreateNewPlaylistBinding? = null
     private val binding get() = _binding!!
 
     private val dialog by lazy { createConfirmPlaylistCreationDialog() }
+
+    private var photoUriToSave: Uri? = null
 
     private val photoLauncher = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()) { photoUri ->
@@ -33,6 +43,7 @@ class PlaylistCreatorFragment : Fragment() {
                 .load(photoUri)
                 .transform(CenterCrop(), roundedCorners(RADIUS_OF_PLAYLIST_COVER_CORNERS, resources))
                 .into(binding.playlistCover)
+            photoUriToSave = photoUri
         } else {
             Log.d("PhotoLauncher", "No cover selected")
         }
@@ -66,6 +77,7 @@ class PlaylistCreatorFragment : Fragment() {
         binding.createNewPlaylistButton.setOnClickListener {
             val playlistName = binding.playlistNameTextInputEditText.text.toString()
             Toast.makeText(requireContext(), "Плейлист $playlistName создан", Toast.LENGTH_SHORT).show()
+            photoUriToSave?.let { saveCoverToExternalStorage(it) }
             findNavController().navigateUp()
         }
 
@@ -104,6 +116,16 @@ class PlaylistCreatorFragment : Fragment() {
             .setMessage(resources.getString(R.string.all_unsaved_data_will_be_lost))
             .setPositiveButton(resources.getString(R.string.finish), dialogListener)
             .setNeutralButton(resources.getString(R.string.cancel), dialogListener)
+    }
+
+    private fun saveCoverToExternalStorage(uri: Uri) {
+        val filePath = File(requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "PlaylistsCovers")
+        if (!filePath.exists()) filePath.mkdirs()
+        val coverFile = File(filePath, "cover_${filePath.listFiles().size + 1}.jpg")
+        val inputStream = requireContext().contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(coverFile)
+        BitmapFactory.decodeStream(inputStream)
+            .compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
     }
 
     companion object {
